91d669bc7b2f2d94e8f18b8727089411a6eaa1db
Lukas <149614931+LukasKaeb@users.noreply.github.com>
2024-07-29 00:14:41 +0200

    Redirect to login page on Homepage

--- tierlist/src/views/HomeView.vue
+++ tierlist/src/views/HomeView.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="home">
-    <h1>Test Page</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id quidem veritatis perferendis provident aspernatur. Est, voluptate quaerat a quidem accusamus asperiores obcaecati voluptatum, error quia iusto magni inventore doloribus esse neque soluta at officia ea repellat sunt adipisci corrupti maxime perspiciatis tenetur molestiae. Quod vero corporis, enim maxime doloremque eligendi.</p>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id quidem veritatis perferendis provident aspernatur. Est, voluptate quaerat a quidem accusamus asperiores obcaecati voluptatum, error quia iusto magni inventore doloribus esse neque soluta at officia ea repellat sunt adipisci corrupti maxime perspiciatis tenetur molestiae. Quod vero corporis, enim maxime doloremque eligendi.</p>
+    <h1>Create your own Tierlist</h1>
   </div>
+  <button @click="redirect" class="login-button">Login</button>
 </template>
 
-<script>
+<script setup>
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
 
-export default {
-  name: 'HomeView',
-  components: { }
+const redirect = () => {
+  router.push("/login")
 }
+
+
 </script>
+
+<style>
+.login-button{
+  width: 80px;
+  height: 40px;
+  background: transparent;
+  border-radius: 5px;
+  cursor: pointer;
+}
+</style>
